f49ddeacd522a11fff3838653a69e27e7f5a7512
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2026-05-12 10:44:36 +0300

    sync lock file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
-  export default function Footer() {
-    return (
-      <footer className="bg-white border-t border-gray-200 py-4 px-6 pb-24 text-center text-sm text-gray-400">
-        © {new Date().getFullYear()} Etijah. All rights reserved.
-      </footer>
-    )
-  }
+export default function Footer() {
+  return (
+    <footer className="bg-white border-t border-gray-200 py-4 px-6 pb-24 text-center text-sm text-gray-400">
+      © {new Date().getFullYear()} Etijah. All rights reserved.
+    </footer>
+  )
+}
